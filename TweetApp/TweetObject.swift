f35debf548d85384c8fdf75cc9bcb9f7f84e437f
Popov Valery <v.popov@rambler-co.ru>
2016-10-29 23:33:47 +0300

    Added tweet managed object model

---
+++ TweetApp/TweetObject.swift
@@ -0,0 +1,17 @@
+//
+//  TweetObject.swift
+//  TweetApp
+//
+//  Created by Popov Valery on 29/10/2016.
+//  Copyright © 2016 Popov Valery. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class TweetObject: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
